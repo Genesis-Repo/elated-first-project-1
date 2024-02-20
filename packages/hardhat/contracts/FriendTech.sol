9cf46bea395c4ebe625b47ff0ac54f0506f09362
mike-menyala <159994059+mike-menyala@users.noreply.github.com>
2024-02-20 17:19:16 +0800

    commit message for multiple files

--- packages/hardhat/contracts/FriendTech.sol
+++ packages/hardhat/contracts/FriendTech.sol
@@ -10,6 +10,9 @@ contract FriendTech is ERC20, AccessControl {
 
     mapping(address => uint256) private sharePrice;
     mapping(address => uint256) public totalShares;
+    mapping(address => uint256) private dividendBalance; // Track dividend balance of each shareholder
+
+    event DividendDistributed(uint256 amount, uint256 totalShareholders);
 
     constructor() ERC20("FriendTech", "FTK") {
         _setupRole(OWNER_ROLE, msg.sender);
@@ -32,6 +35,29 @@ contract FriendTech is ERC20, AccessControl {
 
     function getTotalShares(address user) public view returns (uint256) {
         return totalShares[user];
+    }
+
+    function distributeDividends(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than zero");
+
+        uint256 totalShareholders = 0;
+        for (uint256 i = 0; i < totalSupply(); i++) {
+            address shareholder = tokenOfOwnerByIndex(address(this), i);
+            if (balanceOf(shareholder) > 0) {
+                dividendBalance[shareholder] += (balanceOf(shareholder) * amount) / totalSupply();
+                totalShareholders++;
+            }
+        }
+        emit DividendDistributed(amount, totalShareholders);
+    }
+
+    function withdrawDividends() external {
+        uint256 amount = dividendBalance[msg.sender];
+        require(amount > 0, "No dividends available for withdrawal");
+
+        dividendBalance[msg.sender] = 0;
+        // Perform the transfer of dividends to the shareholder
+        _transfer(address(this), msg.sender, amount);
     }
 
     function buyShares(address seller, uint256 amount) external payable {
